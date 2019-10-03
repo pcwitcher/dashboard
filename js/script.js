921f4c35323ced7c656575836eb5fafb71f03980
PCWitcher <pcwitcher.ie@gmail.com>
2019-10-03 13:15:15 +0100

    styles sidebar

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function toggleMenu(visible) {
+  document.querySelector('.menu').classList.toggle('show', visible);
+}
+
+document.querySelector('.hamburger').addEventListener('click', function(e) {
+  e.preventDefault();
+  toggleMenu();
+});
